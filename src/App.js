3485f0298e8aa1352f48e98f1c27290273b55dd2
mjkleben <mjkleben@gmail.com>
2018-06-11 11:17:04 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import LoginForm from "./components/LoginForm";
+import UserPage from "./components/UserPage";
+import fire from "./components/firebaseInfo";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    };
+  }
+
+  componentDidMount() {
+    this.authListener();
+  }
+
+  authListener = () => {
+    fire.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ user });
+        localStorage.setItem("user", user.uid);
+      } else {
+        this.setState({ user: null });
+        localStorage.removeItem("user");
+      }
+    });
+  };
+
+  render() {
+    console.log("hello world");
+    return (
+      <div>
+        {this.state.user && <UserPage />}
+        {this.state.user === null && <LoginForm />}
+      </div>
+    );
+  }
+}
+
+export default App;
